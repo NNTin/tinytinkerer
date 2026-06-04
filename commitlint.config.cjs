91ee2f69d191b37b61c323bf6b4f2955a72fb318
NNTin <nguyen.ngoctindaniel@gmail.com>
2026-06-04 18:06:21 +0200

    fix: conventional commit message catches Merge ... and Revert ...

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,8 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  defaultIgnores: false,
+  ignores: [
+    (message) =>
+      /^chore\(merge\): merge [0-9a-f]{40} into [0-9a-f]{40}$/m.test(message)
+  ]
 }
