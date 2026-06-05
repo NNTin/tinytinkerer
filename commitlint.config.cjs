82930993e1d24fcdd2c04f2e1f7b1fda59d6b98b
NNTin <nguyen.ngoctindaniel@gmail.com>
2026-06-05 18:35:41 +0200

    feat: reverting commits automatically create the commit message

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -4,5 +4,24 @@ module.exports = {
   ignores: [
     (message) =>
       /^chore\(merge\): merge [0-9a-f]{40} into [0-9a-f]{40}$/m.test(message)
-  ]
+  ],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test'
+      ]
+    ]
+  }
 }
